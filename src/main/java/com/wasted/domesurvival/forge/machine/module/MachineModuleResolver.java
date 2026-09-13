52f8d6c2a74b2d813356e3f9ae54bc3903a2f736
elevenight <pekuepuke1@gmail.com>
2026-09-13 15:59:46 +0400

    feat(modules): add standard item resolver

--- src/main/java/com/wasted/domesurvival/forge/machine/module/MachineModuleResolver.java
+++ src/main/java/com/wasted/domesurvival/forge/machine/module/MachineModuleResolver.java
@@ -5,6 +5,13 @@ import org.jetbrains.annotations.Nullable;
 
 @FunctionalInterface
 public interface MachineModuleResolver {
+    MachineModuleResolver STANDARD = stack -> {
+        if (stack == null || stack.isEmpty()) {
+            return null;
+        }
+        return stack.getItem() instanceof MachineModuleItem moduleItem ? moduleItem.module() : null;
+    };
+
     @Nullable
     MachineModule resolve(ItemStack stack);
 }
